a9dd42d0a16d4e158985cbadb8be5be14b39aac1
LeTadas <legotatadas@gmail.com>
2020-11-23 13:06:57 +0100

    Initial commit

---
+++ TreeAdoption/Authorization/LoginView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationView {
+            VStack {}
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ TreeAdoption/Onboarding/OnboardingView.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        NavigationView {
+            ZStack(alignment: .center) {
+                Text("onboarding_view_title")
+                    .font(.system(size: 45, weight: .bold))
+                    .multilineTextAlignment(.center)
+                    .frame(maxWidth: 307)
+                    .foregroundColor(.white)
+                VStack(alignment: .trailing) {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        NavigationLink(destination: LoginView()) {
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color("primaryColor"))
+                                    .cornerRadius(20)
+                                HStack {
+                                    Text("onboarding_view_begin_button_title")
+                                        .font(.system(size: 16, weight: .bold))
+                                        .foregroundColor(.white)
+                                    Image(systemName: "arrow.forward.circle.fill")
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            .frame(width: 160, height: 40)
+                            .padding(.trailing, 24)
+                        }
+                    }
+                }
+            }
+            .background(
+                Image("background")
+                    .resizable()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+            )
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
