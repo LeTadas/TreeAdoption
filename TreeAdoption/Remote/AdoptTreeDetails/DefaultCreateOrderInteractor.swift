6a9369cfed81459fd5827173b22af27b9b1dd06b
LeTadas <legotatadas@gmail.com>
2020-12-01 14:17:20 +0100

    Create order network layer implementation

---
+++ TreeAdoption/Remote/AdoptTreeDetails/DefaultCreateOrderInteractor.swift
@@ -0,0 +1,44 @@
+import Combine
+import Foundation
+
+protocol CreateOrderInteractor {
+    func createOrder(userId: Int, productId: Int) -> AnyPublisher<Result<WebCreateOrderResponse, RequestError>, Never>
+}
+
+class DefaultCreateOrderInteractor: CreateOrderInteractor {
+    private let networkClient: NetworkClient
+
+    init(_ networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+
+    func createOrder(userId: Int, productId: Int) -> AnyPublisher<Result<WebCreateOrderResponse, RequestError>, Never> {
+        let url = URL(string: "\(ApiConfig.url)/order")
+
+        guard let requestUrl = url else {
+            fatalError("Could not parse url DefaultAvailableDetailsProvider")
+        }
+
+        var urlRequest = URLRequest(url: requestUrl)
+        urlRequest.httpMethod = "POST"
+
+        let newOrder = NewOrderRequest(
+            userId: userId,
+            orderLines: [
+                OrderLine(
+                    productId: productId,
+                    quantity: 1
+                )
+            ]
+        )
+
+        guard let jsonData = try? JSONSerialization.data(
+            withJSONObject: newOrder, options: []
+        ) else {
+            fatalError("Could not serialize json body DefaultCreateOrderInteractor")
+        }
+
+        urlRequest.httpBody = jsonData
+        return networkClient.execute(url: urlRequest)
+    }
+}
