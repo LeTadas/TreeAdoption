1c61ac2e95a808ecdb7262fb429d6ac3831081e3
LeTadas <legotatadas@gmail.com>
2021-01-09 18:51:21 +0100

    Schedule visit implementation

--- TreeAdoption/Remote/AdoptTreeDetails/DefaultCreateOrderInteractor.swift
+++ TreeAdoption/Remote/AdoptTreeDetails/DefaultCreateOrderInteractor.swift
@@ -7,9 +7,11 @@ protocol CreateOrderInteractor {
 
 class DefaultCreateOrderInteractor: CreateOrderInteractor {
     private let networkClient: NetworkClient
+    private let tokenArchiver: TokenArchiver
 
     init(_ networkClient: NetworkClient) {
         self.networkClient = networkClient
+        tokenArchiver = TokenArchiver()
     }
 
     func createOrder(userId: Int, productId: Int) -> AnyPublisher<Result<WebCreateOrderResponse, RequestError>, Never> {
@@ -38,6 +40,13 @@ class DefaultCreateOrderInteractor: CreateOrderInteractor {
         }
 
         urlRequest.httpBody = jsonData
+
+        guard let token = tokenArchiver.getAccessToken() else {
+            fatalError("Auth token is nil")
+        }
+
+        urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+
         return networkClient.execute(url: urlRequest)
     }
 }
--- TreeAdoption/Remote/ScheduleVisit/Tour/WebBookTourService.swift
+++ TreeAdoption/Remote/ScheduleVisit/Tour/WebBookTourService.swift
@@ -4,17 +4,19 @@ import Foundation
 class WebBookTourService {
     private let networkClient: NetworkClient
     private let tokenArchiver: TokenArchiver
+    private let userPersister: UserPersister
 
     init(_ networkClient: NetworkClient) {
         self.networkClient = networkClient
         tokenArchiver = TokenArchiver()
+        userPersister = UserPersister()
     }
 
-    func bookATour(id: Int) -> AnyPublisher<Result<WebForestResponse, RequestError>, Never> {
-        let url = URL(string: "\(ApiConfig.url)/forest/\(id)")
+    func bookATour(tourId: Int) -> AnyPublisher<Result<BookTourResponse, RequestError>, Never> {
+        let url = URL(string: "\(ApiConfig.url)/bookedtour")
 
         guard let requestUrl = url else {
-            fatalError("Could not parse url DefaultNewsProvider")
+            fatalError("Could not parse url WebBookTourService")
         }
 
         var urlRequest = URLRequest(url: requestUrl)
@@ -23,15 +25,26 @@ class WebBookTourService {
             fatalError("Auth token is nil")
         }
 
+        guard let user = userPersister.getUser() else {
+            fatalError("User is nil")
+        }
+
         urlRequest.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        urlRequest.httpMethod = "POST"
 
         let request = BookTourRequest(
-            tourId: 0,
-            userId: 0,
-            userName: "",
-            userEmail: "",
-            bookedDateTime: ""
+            tourId: tourId,
+            userId: user.id,
+            userName: user.firstName,
+            userEmail: user.email,
+            bookedDateTime: DateFormatter.localDateTime.string(from: Date())
         )
+
+        guard let jsonData = try? JSONEncoder().encode(request) else {
+            fatalError("Could not serialize json body WebBookTourService")
+        }
+
+        urlRequest.httpBody = jsonData
 
         return networkClient.execute(url: urlRequest)
     }
